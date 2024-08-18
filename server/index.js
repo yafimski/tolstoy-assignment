d2041a2c52051332ef4975f1304e62f6428a37e8
yafimski <yafimski@gmail.com>
2024-08-18 17:11:59 +0300

    server client from index.js

--- server/index.js
+++ server/index.js
@@ -11,6 +11,11 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
 app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
